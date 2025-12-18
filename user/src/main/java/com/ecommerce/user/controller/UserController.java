11a2c6cc16af5ba46c908fa30d4555f578e0c8ce
Grazielle <grazielle.anaia@gmail.com>
2025-12-18 15:09:28 -0500

    Added configuration for getting logs in config server

--- user/src/main/java/com/ecommerce/user/controller/UserController.java
+++ user/src/main/java/com/ecommerce/user/controller/UserController.java
@@ -5,6 +5,9 @@ import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import com.ecommerce.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +17,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-
+@Slf4j
 public class UserController {
+
+//    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService;
 
@@ -33,6 +38,12 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id) {
+//        logger.info("Request received for user: {}", id); //Spring displays info and error logs by default
+//        logger.trace("This is TRACE level"); //Spring hides trace and debug level unless they are enabled
+//        logger.debug("Used for development debugging");
+//        logger.error("error level logger");
+
+        log.info("Request received for user: {}", id);
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
