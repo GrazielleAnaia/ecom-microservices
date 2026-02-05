95fae65dec94eb34e3da921ed16b28029e8d0903
Grazielle <grazielle.anaia@gmail.com>
2026-02-05 12:27:27 -0500

    added build images from dockerfile and buildpacks to folder deploy

---
+++ deploy/docker/build-images-buildpacks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# Build all services
+
+cd ../..
+
+cd eureka && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd gateway && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd configserver && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd order && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd user && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd product && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd notification && ./mvnw spring-boot:build-image -DskipTests && cd ..
